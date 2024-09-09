e673557a90f887dd0f8fa9241eb4c090b671cb4f
chandansgowda <chandansuresh007@gmail.com>
2024-09-10 02:03:57 +0530

    initial commit

---
+++ 2.py
@@ -0,0 +1,21 @@
+name = "chandan" #string
+age = 22 #int
+is_student = False # bool
+weight = 69.5 #float
+
+print(type(is_student))
+
+s = "100"
+print(int(s)+age)
+
+a = 10
+b = 3
+print(a + b)  # Output: 13
+print(a - b)  # Output: 7
+print(a * b)  # Output: 30
+print(a / b)  # Output: 3.3333...
+print(a // b)  # Output: 3 (Floor Division)
+print(a % b)  # Output: 1 (Modulus|Remainder)
+print(a ** b)  # Output: 1000 (Exponentiation)
+
+print(a+b-a+b*2**3)
